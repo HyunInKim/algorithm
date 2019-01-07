500f196e10b7270af0b0047205d656e2ea419f8a
HyunIn <appkimhyunin@gmail.com>
2019-01-07 14:31:05 +0900

    프로그래머스 - 약수의 합 - JAVA

---
+++ src/SumOfMeasure/Solution.java
@@ -0,0 +1,21 @@
+package SumOfMeasure;
+class solutions{
+	public int solution (int n) {
+		int answer = 0;
+		for(int i=1;i<=n;i++) {
+			if(n%i== 0) {
+				answer = answer + i;
+			}
+		}
+		return answer;
+	}
+}
+public class Solution {
+	public static void main(String[] args) {
+		solutions ss = new solutions();
+		System.out.println(ss.solution(12));
+		
+
+	}
+
+}
