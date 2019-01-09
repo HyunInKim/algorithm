573fdc24cc831252b4812cf15b5412e7f3530ce3
HyunIn <appkimhyunin@gmail.com>
2019-01-09 17:41:01 +0900

    프로그래머스  - 소수 찾기 - JAVA (에라토스테네스 의 체 알고리즘 사용)

--- src/Prime_number/Prime.java
+++ src/Prime_number/Prime.java
@@ -2,27 +2,24 @@ package Prime_number;
 import java.util.*;
 class Solution{
 	public int solution(int n) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
+		boolean[] b = new boolean[n+1];
+		int count = 0;
 		for(int i=2;i<=n;i++) {
-			int count = 0;
-			for(int j=1;j<=n;j++) {
-				if(i%j==0) {
-					count+=1;
+			if(b[i] == false) {
+				count +=1;
+				for(int j=i;j<=n;j+=i) {
+					b[j] = true;				
 				}
-			}
-			if(count<=2) {
-				list.add(i);
-			}
-		}
-		return list.size();
+			}			
+		}		
+		return count;
 	}
 }
 public class Prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Solution ss = new Solution();
-		System.out.println(ss.solution(100));
+		System.out.println(ss.solution(10000000));
 		
 	}
 
